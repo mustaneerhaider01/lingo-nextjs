a126322c5c448a638571fd2cd043ed09f2de2979
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-07-18 19:27:14 +0500

    Added German

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -51,13 +51,13 @@ export const Footer = () => {
 
         <Button size="lg" variant="ghost" className="w-full">
           <Image
-            src="/jp.svg"
-            alt="Japanese"
+            src="/de.svg"
+            alt="German"
             height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Japanese
+          German
         </Button>
       </div>
     </footer>
