769ab7bdb0b984263b14a0397306b4efa6104309
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-07-19 16:36:47 +0500

    Added new tables

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,22 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import UserProgress from "@/components/user-progress";
+import { Header } from "./components/header";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-12 px-6">
-      <StickyWrapper>My sticky header</StickyWrapper>
-      <FeedWrapper>My feed</FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={10}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
-import { Footer } from "./_components/footer";
-import { Header } from "./_components/header";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
 
 type Props = {
   children: React.ReactNode;
